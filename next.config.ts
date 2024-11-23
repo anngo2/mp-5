1acfe374ca0f84be37dbca72675136cd9b4ca89e
Sue Ngo <suengo@Sues-MacBook-Pro.local>
2024-11-22 19:29:42 -0500

    pushing code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
 
+module.exports = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
-export default nextConfig;
+
